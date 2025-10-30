459bfe16989f197d8883978aff02b1009a16b4d7
Reuben <reuben534@gmail.com>
2025-10-30 18:55:38 +0200

    update

--- script.js
+++ script.js
@@ -108,10 +108,7 @@ function initContactForm() {
     // Show loading state
     submitButton.disabled = true;
     submitButton.innerHTML = `
-      <svg class="animate-spin h-5 w-5 mr-2 inline" viewBox="0 0 24 24" fill="none" stroke="currentColor">
-        <circle class="opacity-25" cx="12" cy="12" r="10" stroke-width="4"></circle>
-        <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
-      </svg>
+      <i class="fas fa-spinner fa-spin mr-2"></i>
       Sending...
     `;
 
